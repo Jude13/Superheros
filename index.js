d3d8766e71c4059804b85185d8ee2c5190cd7c21
Jude13 <40498314+Jude13@users.noreply.github.com>
2018-06-22 09:31:01 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function myFunction() {
+  var checkBox = document.getElementById('myCheck');
+if (checkBox.checked == true){
+  alert('Nice sounds like you are ready!');
+}
+
+}
+
+
+function myFunction2() {
+  var checkBox = document.getElementById('myCheck2');
+  if (checkBox.checked == true){
+    alert ('Its alright you cant always be perfect!');
+  }
+}
+
+
+function myFunction3() {
+  var checkBox = document.getElementById('myCheck3');
+  if (checkBox.checked == true){
+    alert ('At least your going to put in some effort :)');
+  }
+}
